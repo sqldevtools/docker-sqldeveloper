284744412fab950b50739922d4aab7018277211b
Martin Passing <github@passing.name>
2016-03-13 12:59:16 +0100

    remove wrong touch

--- run.sh
+++ run.sh
@@ -10,7 +10,6 @@ fi
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 
-touch $XSOCK
 xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 docker run -ti -v $CONFIG:/root -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH sqldeveloper
