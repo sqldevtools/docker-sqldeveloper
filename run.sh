9bda7959adb1473f4c2335dbdf068d20b855f7f7
Martin Passing <github@passing.name>
2016-03-14 23:26:43 +0100

    use tnsnames.ora file from /etc

--- run.sh
+++ run.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 CONFIG=~/.sqldeveloper
+TNSNAMES=/etc/tnsnames.ora
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.${USER}.xauth
 
 if [ ! -d "$CONFIG" ]
 then
 	mkdir "$CONFIG"
 fi
 
-XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.${USER}.xauth
+if [ -f $TNSNAMES ]
+then
+	TNS="-v $TNSNAMES:$TNSNAMES"
+fi
 
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-docker run -ti -v $CONFIG:/root -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH sqldeveloper
+docker run -ti $TNS -v $CONFIG:/root -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH sqldeveloper
 
